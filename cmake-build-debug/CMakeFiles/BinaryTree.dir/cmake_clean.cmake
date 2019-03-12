f1eca4f9f29aec1ef9ef13180d90dfe643aa5eb9
Jimmy <greatlodeman@gmail.com>
2019-03-12 13:59:31 -0600

    Main and other files for Clion IDE

---
+++ cmake-build-debug/CMakeFiles/BinaryTree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BinaryTree.dir/main.c.o"
+  "BinaryTree.pdb"
+  "BinaryTree.exe"
+  "libBinaryTree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BinaryTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
