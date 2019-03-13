1864b690960d98812b3f978950752d335c400e0d
Alec Richardson <alec.richardson@umontana.edu>
2019-03-12 20:01:15 -0600

    Update binarytree.c

--- binarytree.c
+++ binarytree.c
@@ -7,6 +7,29 @@ void initialize(binary_tree* bt) {
     bt->size = 0;
 }
 
+bool search(binary_tree* bt, int key) {
+     Node* current = bt->root;
+
+     while(current != NULL) {
+         
+         //returns true if key is found
+         if(key == current->key) {
+             return true;
+         }
+         //goes left on tree if key is smaller than current nodes key
+         else if(key < current->key) {
+             current = current->nextNodeLeft;
+         }
+         //goes right if key is bigger than current nodes key
+         else {
+             current = current->nextNodeRight;
+         }
+     }
+
+     //returns false if key is not found
+     return false;
+}
+
 int btSize(binary_tree* bt) {
     return bt->size;
 }
