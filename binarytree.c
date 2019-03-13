3115f356d655bd5514dc3bd9961e9f841fa5bd0b
Alec Richardson <alec.richardson@umontana.edu>
2019-03-13 00:40:36 -0600

    Completed insert function

--- binarytree.c
+++ binarytree.c
@@ -30,6 +30,58 @@ bool search(binary_tree* bt, int key) {
      return false;
 }
 
+void insert(binary_tree* bt, int key) {
+    Node* new = (Node*)malloc(sizeof(Node));
+    new->key = key;
+    new->nextNodeLeft = NULL;
+    new->nextNodeRight = NULL;
+
+    //if tree is empty
+    if(bt->root == NULL) {
+        bt->root = new;
+    }
+    //if tree contains nodes
+    else {
+       Node* current = bt->root;
+       //previous will be the parent node after loop is completed
+       Node* previous;
+       
+       //loops until it reaches place of insertion
+       while(current != NULL) {
+           //checks if key goes left of current node
+           if(key < current->key) {
+               previous = current;
+               current = current->nextNodeLeft;
+           }
+           //checks if key goes right of current node
+           else if(key > current->key) {
+               previous = current;
+               current = current->nextNodeRight;
+           }
+       }
+       //deals with the case of the root being the only node in the tree
+       if (current = bt->root) {
+           if(key < bt->root->key) {
+               current = bt->root->nextNodeLeft;  
+           }
+           else if(key > bt->root->key) {
+               current = bt->root->nextNodeRight;
+           }
+       }
+       
+       //adds the new node to the right or left of parent node
+       if (key < previous->key) {
+		    previous->nextNodeLeft = new;
+       }
+	    else{
+		    previous->nextNodeRight = new;
+        }
+    }
+    bt->size++;
+}
+
+
+
 int btSize(binary_tree* bt) {
     return bt->size;
 }
