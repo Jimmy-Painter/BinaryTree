0f52cf3fa503f5ec81630eec65c2cd89058fc5a0
Alec Richardson <alec.richardson@umontana.edu>
2019-03-17 00:54:03 -0600

    fixed main. Whole thing is working now

--- main.c
+++ main.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include "binarytree.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+   
+   //gives error and returns 1 if # command line arguments is wrong 
+    if(argc != 2) {
+        printf("Too many arguments\n");
+        return 1;
+    }
 
-    FILE* numbers = fopen("C:\\Users\\Jimmy\\Documents\\GitHub\\BinaryTree\\myfile.txt", "r");
+    FILE* numbers = fopen(argv[1], "r");
     binary_tree bt;
-    int found;
+    bool found;
     int const ARR_SIZE = 10;
     int nums[ARR_SIZE ];
     int i;
     int numRead;
 
-    while (!feof(numbers)){
-        fscanf(numbers, "%d", &numRead);
-        printf("%d\n", numRead);
-        insert(&bt, numRead); 
+    //error if file can't be opened
+    if(numbers == NULL) {
+        printf("File could not be opened\n");
+        return 1;
     }
 
     initialize(&bt);
 
+    //read content of files and insert into bt
+    while (!feof(numbers)){
+        fscanf(numbers, " %d", &numRead);
+        insert(&bt, numRead); 
+    }
 
     printf("Print in order\n");
     printinorder(&bt);
@@ -38,26 +49,7 @@ int main() {
     printf("Number of elements in tree: %d\n", btsize(&bt));
     printf("Tree height: %d\n", treeheight(&bt));
 
+   return 0;
 
-
-    printf("Print in order\n");
-    printinorder(&bt);
-    printf("\nPrint pre order\n");
-    printpreorder(&bt);
-    printf("\nPrint post order\n");
-    printpostorder(&bt);
-    printf("\n");
-    found = search(&bt,33);
-    if (found){
-        printf("FOUND 33\n");
-    }
-    found = search(&bt,38);
-    if (found){
-        printf("FOUND 38\n");
-    }
-    printf("Number of elements in tree: %d\n", btsize(&bt));
-    printf("Tree height: %d\n", treeheight(&bt));
-
-
-    return 0;
+    
 }
